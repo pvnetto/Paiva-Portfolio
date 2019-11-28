871bad16caa646a4c32b854eaf1d991cac1e773a
Paiva <pvnetto1@gmail.com>
2019-11-28 20:52:33 -0300

    add fontawesome

--- src/components/sidenav/Sidenav.js
+++ src/components/sidenav/Sidenav.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import style from './sidenav.module.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faCode, faFistRaised, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Sidenav = () => {
 
     return (
-        <div className={style.sidenav}></div>
+        <div className={style.sidenav}>
+            <div>
+                <p>P</p>
+            </div>
+            <div className={style.sidenavBtnsContainer}>
+                <a className={style.sidenavBtn} href=""><FontAwesomeIcon icon={faHome} /></a>
+                <a className={style.sidenavBtn} href=""><FontAwesomeIcon icon={faUser} /></a>
+                <a className={style.sidenavBtn} href=""><FontAwesomeIcon icon={faFistRaised} /></a>
+                <a className={style.sidenavBtn} href=""><FontAwesomeIcon icon={faCode} /></a>
+                <a className={style.sidenavBtn} href=""><FontAwesomeIcon icon={faEnvelope} /></a>
+            </div>
+
+            <div className={style.sidenavBtnsContainer}>
+                <a className={style.socialMediaBtn} href=""><FontAwesomeIcon icon={faFacebookF} /></a>
+                <a className={style.socialMediaBtn} href=""><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                <a className={style.socialMediaBtn} href=""><FontAwesomeIcon icon={faGithub} /></a>
+            </div>
+        </div>
     );
 };
 
