757de3360aa273b8c1778f65f8f0a64beb8e952e
Paiva <pvnetto1@gmail.com>
2019-12-16 08:36:55 -0300

    test fix mobile drag

--- src/components/pages/projects/info.js
+++ src/components/pages/projects/info.js
@@ -31,19 +31,12 @@ export const projectsInfo = [
             `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
             Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
             justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
-        ],
-        awards: []
-    },
-    {
-        id: 1,
-        title: 'titoru',
-        category: categories.GAMES,
-        techs: [techs.UNITY3D, techs.PYTHON],
-        roles: [roles.PROGRAMMER],
-        thumbnail: [],
-        description: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
-            Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
+            ,
+
+            `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
+            Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
+            justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
+            ,
 
             `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
             Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
@@ -51,55 +44,72 @@ export const projectsInfo = [
         ],
         awards: []
     },
-    {
-        id: 2,
-        title: 'titoru',
-        category: categories.GAMES,
-        techs: [techs.UNITY3D, techs.PYTHON, techs.JAVASCRIPT, techs.REACT],
-        roles: [roles.PROGRAMMER],
-        thumbnail: [],
-        description: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
-            Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
+    // {
+    //     id: 1,
+    //     title: 'titoru',
+    //     category: categories.GAMES,
+    //     techs: [techs.UNITY3D, techs.PYTHON],
+    //     roles: [roles.PROGRAMMER],
+    //     thumbnail: [],
+    //     description: [
+    //         `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
+    //         Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
 
-            `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
-            Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
-            justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
-        ],
-        awards: []
-    },
-    {
-        id: 3,
-        title: 'titoru',
-        category: categories.GAMES,
-        techs: [techs.UNITY3D],
-        roles: [roles.PROGRAMMER],
-        thumbnail: [],
-        description: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
-            Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
+    //         `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
+    //         Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
+    //         justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
+    //     ],
+    //     awards: []
+    // },
+    // {
+    //     id: 2,
+    //     title: 'titoru',
+    //     category: categories.GAMES,
+    //     techs: [techs.UNITY3D, techs.PYTHON, techs.JAVASCRIPT, techs.REACT],
+    //     roles: [roles.PROGRAMMER],
+    //     thumbnail: [],
+    //     description: [
+    //         `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
+    //         Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
 
-            `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
-            Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
-            justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
-        ],
-        awards: []
-    },
-    {
-        id: 4,
-        title: 'titoru',
-        category: categories.GAMES,
-        techs: [techs.UNITY3D],
-        roles: [roles.PROGRAMMER],
-        thumbnail: [],
-        description: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
-            Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
+    //         `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
+    //         Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
+    //         justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
+    //     ],
+    //     awards: []
+    // },
+    // {
+    //     id: 3,
+    //     title: 'titoru',
+    //     category: categories.GAMES,
+    //     techs: [techs.UNITY3D],
+    //     roles: [roles.PROGRAMMER],
+    //     thumbnail: [],
+    //     description: [
+    //         `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
+    //         Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
 
-            `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
-            Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
-            justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
-        ],
-        awards: []
-    },
+    //         `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
+    //         Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
+    //         justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
+    //     ],
+    //     awards: []
+    // },
+    // {
+    //     id: 4,
+    //     title: 'titoru',
+    //     category: categories.GAMES,
+    //     techs: [techs.UNITY3D],
+    //     roles: [roles.PROGRAMMER],
+    //     thumbnail: [],
+    //     description: [
+    //         `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod suscipit velit, ut condimentum arcu fermentum in.
+    //         Quisque egestas facilisis ante et convallis. Pellentesque a nunc in ligula placerat consectetur.`,
+
+    //         `In luctus ac ex vel sollicitudin. Aenean in tortor vel nulla efficitur efficitur sit amet ut orci.
+    //         Sed dignissim diam purus, vel aliquam erat tincidunt at. In a orci nunc. Praesent nec risus sit amet
+    //         justo venenatis molestie non nec sapien. In commodo maximus nibh, vitae porttitor mauris efficitur sed.`
+    //     ],
+    //     awards: []
+    // },
 ]
