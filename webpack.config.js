ef1643a33fac636865ffe3d0c895537c3e8812cd
Paiva <pvnetto1@gmail.com>
2019-12-19 12:57:54 -0300

    fix environment variables setup on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 const baseConfig = require('./webpack.config.base');
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
 
 module.exports = {
     ...baseConfig,
@@ -7,5 +16,9 @@ module.exports = {
     devServer: {
         contentBase: './dist',
         historyApiFallback: true,
-    }
+    },
+    plugins: [
+        ...baseConfig.plugins,
+        new webpack.DefinePlugin(envKeys)
+    ]
 }
