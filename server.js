a455fbd6082d6be6ea147470fd3951dfbfde2c67
Paiva <pvnetto1@gmail.com>
2019-12-19 13:02:42 -0300

    add dotenv to server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.static(__dirname + '/dist'));
